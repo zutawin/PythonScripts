0fa186cde186c77069130182504598348d6ccc8b
zutawin <zutawin.chojo@outlook.com>
2019-02-14 23:12:11 +0800

    Create mqttClientExample.py
    example interfacing mqtt using python

---
+++ mqttClientExample.py
@@ -0,0 +1,39 @@
+import os, sys, time
+import paho.mqtt.client as mqtt
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    #client.subscribe("$SYS/#")
+    client.subscribe("/esp8266/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+#broker_address = "rpi3z.local"
+#broker_address = "bluerpi.local"
+broker_address = "Ubuntu18041-VirtualBox.local"
+client.connect(broker_address, 1883, 60)
+
+
+while(1):
+    client.publish("/esp8266/inTopic", "0")
+    time.sleep(0.5)
+    client.publish("/esp8266/inTopic", "1")
+    time.sleep(0.5)
+    client.loop()
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+
+#client.loop_forever()
