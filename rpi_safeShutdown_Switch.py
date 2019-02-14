0aa3e93ba5e1c2becf2c9d076c977ee4a8cae9c1
zutawin <zutawin.chojo@outlook.com>
2019-02-14 23:12:39 +0800

    Create rpi_safeShutdown_Switch.py
    safe shutdown python script for rpi

---
+++ rpi_safeShutdown_Switch.py
@@ -0,0 +1,17 @@
+from gpiozero import button #import button from the Pi GPIO library
+import time # import time functions
+import os #imports OS library for Shutdown control
+
+stopButton = Button(26) # defines the button as an object and chooses GPIO 26
+
+while True: #infinite loop
+     if stopButton.is_pressed: #Check to see if button is pressed
+        time.sleep(1) # wait for the hold time we want. 
+        if stopButton.is_pressed: #check if the user let go of the button
+            os.system("shutdown now -h") #shut down the Pi -h is or -r will reset
+    time.sleep(1) # wait to loop again so we don’t use the processor too much.
+
+
+
+#bash
+# cat sudo python /home/pi/scripts/rpi_safeShutdown_switch.py & >> /etc/rc.local
